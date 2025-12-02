c063daae0f5d0d9a428a8700e39d8cb356928b23
nachoryu <1113293@sktelecom.com>
2025-12-02 09:25:50 +0900

    going

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import './App.css';
-
-import {Route, Routes, Navigate} from 'react-router-dom';
+import Router from 'components/Router';
+import { Layout } from 'components/Layout';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/posts" element={<h1>Post List</h1>} />
-      <Route path="/posts/:id" element={<h1>Post Detail</h1>} />
-      <Route path="/posts/new" element={<h1>Post New</h1>} />
-      <Route path="/posts/edit/:id" element={<h1>Post Edit</h1>} />
-      <Route path="/profile" element={<h1>Profile</h1>} />
-      <Route path="profile/edit" element={<h1>Profile Edit</h1>} />
-      <Route path="/notifications" element={<h1>Notifications</h1>} />
-      <Route path="/search" element={<h1>Search</h1>} />
-      <Route path="/users/login" element={<h1>Login</h1>} />
-      <Route path="/users/signup" element={<h1>Signup</h1>} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <Layout>
+      <Router />
+    </Layout>
+    
   );
 }
 
